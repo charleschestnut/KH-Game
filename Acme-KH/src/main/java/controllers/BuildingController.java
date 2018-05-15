cd35c398aaee4db62f72c364aba23c687bd814c8
agepere <36485821+agepere@users.noreply.github.com>
2018-05-15 19:09:05 +0200

    Controlador de building para user hecho

---
+++ Acme-KH/src/main/java/controllers/BuildingController.java
@@ -0,0 +1,96 @@
+
+package controllers;
+
+import java.util.Collection;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import services.BuildingService;
+import services.DefenseService;
+import services.LivelihoodService;
+import services.RecruiterService;
+import services.RequirementService;
+import services.WarehouseService;
+import domain.Building;
+import domain.Defense;
+import domain.Livelihood;
+import domain.Recruiter;
+import domain.Warehouse;
+
+@Controller
+@RequestMapping("/building")
+public class BuildingController extends AbstractController {
+
+	@Autowired
+	private BuildingService		buildingService;
+	@Autowired
+	private DefenseService		defenseService;
+	@Autowired
+	private RecruiterService	recruiterService;
+	@Autowired
+	private WarehouseService	warehouseService;
+	@Autowired
+	private LivelihoodService	livelihoodService;
+	@Autowired
+	private RequirementService	requirementService;
+
+
+	@RequestMapping("/list")
+	public ModelAndView list() {
+		final ModelAndView res;
+		final Collection<Building> buildings = this.buildingService.getAvailableBuildings();
+
+		res = new ModelAndView("building/list");
+		res.addObject("buildings", buildings);
+		res.addObject("requestURI", "building/list.do");
+
+		return res;
+	}
+
+	@RequestMapping("/display")
+	public ModelAndView display(@RequestParam final Integer buildingId) {
+		ModelAndView res = new ModelAndView("building/display");
+		Recruiter recruiter = null;
+		Warehouse warehouse = null;
+		Livelihood livelihood = null;
+
+		final Defense defense = this.defenseService.findOne(buildingId);
+		if (defense != null) {
+			res.addObject("defense", true);
+			res.addObject("building", defense);
+		} else {
+
+			recruiter = this.recruiterService.findOne(buildingId);
+			if (recruiter != null) {
+				res.addObject("recruiter", true);
+				res.addObject("building", recruiter);
+				//TODO: añadir query para las tropas y gummi ships
+
+			} else {
+				livelihood = this.livelihoodService.findOne(buildingId);
+				if (livelihood != null) {
+					res.addObject("livelihood", true);
+					res.addObject("building", livelihood);
+
+				} else {
+					warehouse = this.warehouseService.findOne(buildingId);
+					res.addObject("warehouse", true);
+					res.addObject("building", warehouse);
+
+				}
+
+			}
+		}
+
+		if (warehouse == null && defense == null && recruiter == null && livelihood == null)
+			res = new ModelAndView("redirect:list.do");
+		else
+			res.addObject("requirements", this.requirementService.getRequirementsByBuilding(buildingId));
+
+		return res;
+	}
+}
