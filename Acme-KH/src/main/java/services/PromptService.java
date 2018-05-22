95d09948b097a1947aac82687a95683328215b16
camreyaro <33806215+camreyaro@users.noreply.github.com>
2018-05-22 17:55:36 +0200

    bug fixed

--- Acme-KH/src/main/java/services/PromptService.java
+++ Acme-KH/src/main/java/services/PromptService.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.apache.commons.lang3.StringUtils;
 import org.springframework.transaction.annotation.Transactional;
 
 import security.Authority;
