f12c383dbe79d5c3d13ced8c2bd78aa5a312ebc9
angelxwars <34144181+angelxwars@users.noreply.github.com>
2018-05-26 11:50:39 +0200

    Faction service hecho

--- Acme-KH/src/main/java/services/FactionService.java
+++ Acme-KH/src/main/java/services/FactionService.java
@@ -7,8 +7,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.Assert;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.Validator;
 
 import repositories.FactionRepository;
+import security.Authority;
 import domain.Faction;
 
 @Service
@@ -20,28 +23,38 @@ public class FactionService {
 	@Autowired
 	private FactionRepository	FactionRepository;
 
+	@Autowired
+	private ActorService		actorService;
 
-	// CRUD methods
+	@Autowired
+	Validator					validator;
+
 
 	public Faction create() {
-		Faction Faction;
+		Faction faction;
 
-		Faction = new Faction();
+		faction = new Faction();
+		faction.setExtraAttack(0.0);
+		faction.setExtraDefense(0.0);
+		faction.setExtraResources(0.0);
+		faction.setGalaxy(0);
 
-		return Faction;
+		return faction;
 	}
 
-	public Faction save(Faction Faction) {
+	public Faction save(final Faction Faction) {
 		Assert.notNull(Faction);
 
 		Faction saved;
+
+		Assert.isTrue(this.actorService.findByPrincipal().getUserAccount().getAuthorities().contains(Authority.MANAGER), "message.error.notManager");
 
 		saved = this.FactionRepository.save(Faction);
 
 		return saved;
 	}
 
-	public Faction findOne(int FactionId) {
+	public Faction findOne(final int FactionId) {
 		Assert.notNull(FactionId);
 
 		Faction Faction;
@@ -49,7 +62,6 @@ public class FactionService {
 		Faction = this.FactionRepository.findOne(FactionId);
 
 		Assert.notNull(Faction, "error.message.null");
-
 		return Faction;
 	}
 
@@ -61,10 +73,27 @@ public class FactionService {
 		return Factions;
 	}
 
-	public void delete(Faction Faction) {
+	public void delete(final Faction Faction) {
 		Assert.notNull(Faction);
 
 		this.FactionRepository.delete(Faction);
 	}
+	public Faction reconstruct(final Faction s, final BindingResult binding) {
+		Faction result;
 
+		if (s.getId() == 0)
+			result = s;
+		else {
+			result = this.findOne(s.getId());
+			result.setExtraAttack(s.getExtraAttack());
+			result.setExtraDefense(s.getExtraDefense());
+			result.setExtraResources(s.getExtraResources());
+			result.setName(s.getName());
+
+		}
+
+		this.validator.validate(result, binding);
+
+		return result;
+	}
 }
