ece3fc362ea8890180180af0afe1c33e1588e8e0
angelxwars <34144181+angelxwars@users.noreply.github.com>
2018-05-26 11:49:47 +0200

    MyBattles

--- Acme-KH/src/main/java/repositories/BattleRepository.java
+++ Acme-KH/src/main/java/repositories/BattleRepository.java
@@ -1,10 +1,15 @@
+
 package repositories;
 
+import java.util.Collection;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import domain.Battle;
 
 public interface BattleRepository extends JpaRepository<Battle, Integer> {
-	
 
+	@Query("select b from Battle b where b.attacker.id=?1")
+	public Collection<Battle> myBattles(Integer actorId);
 }
